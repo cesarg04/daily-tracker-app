b10d4e18a1b5943c91c369537f945e63131a7884
cESAR <elduro2002_@hotmail.com>
2024-09-13 14:12:29 -0400

    add-sheet

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -4,13 +4,18 @@ import useModal from '@/shared/hooks/useModal'
 import CustomAlertTemplate from '@/shared/components/alerts/CustomAlertTemplate'
 import useAlert from '@/shared/hooks/useAlert'
 import { useCreateIncome } from '@/private/modules/home/hooks/useCreateInconme'
+import CreateIcomeSheet from '@/private/modules/home/components/create-income/CreateIncomeSheet'
+import { SheetManager } from 'react-native-actions-sheet/dist/src/sheetmanager'
 
 const Home = () => {
 
   const { createIncome } = useCreateIncome()
 
   useEffect(() => {
-    createIncome()
+    // createIncome()
+    SheetManager.show('create-income-sheet').then(res => {
+      console.log(res)
+    });
   }, [])
   
 
---
+++ shared/constants/fonts/fonts.conts.ts
@@ -0,0 +1,9 @@
+export const fontFamilies = {
+  Thin: "MPLUSRounded1c_100Thin",
+  Light: "MPLUSRounded1c_300Light",
+  Regular: "MPLUSRounded1c_400Regular",
+  Medium: "MPLUSRounded1c_500Medium",
+  Bold: "MPLUSRounded1c_700Bold",
+  ExtraBold: "MPLUSRounded1c_800ExtraBold",
+  Black: "MPLUSRounded1c_900Black",
+};
