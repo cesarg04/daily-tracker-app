722cd81c891b53d228ff86713e5225bbf32c560b
cESAR <elduro2002_@hotmail.com>
2024-12-21 18:32:56 -0400

    modify-date-description

---
+++ shared/helpers/parse-date.helper.ts
@@ -0,0 +1,13 @@
+import dayjs from 'dayjs';
+import localeData from 'dayjs/plugin/localeData';
+import 'dayjs/locale/es';
+
+dayjs.extend(localeData);
+dayjs.locale('es');
+
+const formatDateDescription = (dateString: string): string => {
+    const date = dayjs(dateString, 'MM-DD-YYYY');
+    return date.format('D [de] MMMM [de] YYYY');
+};
+
+export default formatDateDescription;
